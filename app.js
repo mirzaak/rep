52718044d69f846a4260d0959d43ce4357d107e3
mirzaak <mirzakaric002@gmail.com>
2021-11-25 02:14:34 +0100

    events

--- app.js
+++ app.js
@@ -1,4 +1,22 @@
 const app = Vue.createApp({
+data(){
+    return{
+        pokaziKnjige: true,
+        title:'Naslov',
+        subtitle:'Podnaslov',
+        broj: 100
+    }
+},
+    methods:{
+        promijeni(){
+            this.pokaziKnjige = !this.pokaziKnjige
+        },
+        toggle(e, d){
+            console.log(e, e.type,d)
+        
+        
+    }
+    }
 
 })
 
