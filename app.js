ffab7ffc492257e5232a405fd5fe9108b61f27b7
mirzaak <mirzakaric002@gmail.com>
2021-11-25 20:50:29 +0100

    krajvjezbe

--- app.js
+++ app.js
@@ -2,20 +2,25 @@ const app = Vue.createApp({
 data(){
     return{
         pokaziKnjige: true,
-        title:'Naslov',
-        subtitle:'Podnaslov',
-        broj: 100
+        knjige:[
+            { naslov: "prvi naslov", pisac: "prvi pisac", img: "assets/bmw.png", naj: true },
+            { naslov: "drugi naslov", pisac: "drugi pisac", img: "assets/benz.jpg", naj: false },
+            { naslov: "treci naslov", pisac: "treci pisac", img: "assets/audi.jpg", naj : true }
+        ]
     }
 },
     methods:{
         promijeni(){
             this.pokaziKnjige = !this.pokaziKnjige
         },
-        toggle(e, d){
-            console.log(e, e.type,d)
-        
-        
-    }
+        toggleFav(knjiga){
+            knjiga.naj = !knjiga.naj
+        }
+    },
+    computed:{
+        filtriraneKnjige(){
+            return this.knjige.filter((knjiga)=>knjiga.naj)
+        }
     }
 
 })
